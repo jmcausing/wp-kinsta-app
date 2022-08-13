409132e45e0150b04842a4bb5aaf00600c450f00
jmcausing <johnmarkcausing@gmail.com>
2022-08-13 08:19:28 +0800

    wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -58,6 +58,10 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
+if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+
+$_SERVER['HTTPS']='on';
+
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){ $protocol = "https://".$_SERVER['HTTP_HOST']; } else{ $protocol='http://'.$_SERVER['HTTP_HOST']; }
 
 	
